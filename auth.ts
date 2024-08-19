8ada42be3c3b069625eab4bc8620853fc01df57f
voidkudo <voidkudo@gmail.com>
2024-08-19 12:40:40 -0400

    fix: auth no return

--- auth.ts
+++ auth.ts
@@ -24,17 +24,15 @@ export const { auth, signIn, signOut } = NextAuth({
         .object({ email: z.string().email(), password: z.string().min(6) })
         .safeParse(credentials);
 
-        if (parsedCredentials.success) {
-          const { email, password } = parsedCredentials.data;
-          const user = await getUser(email);
-          if (!user) return null;
-          const passwordsMatch = await bcrypt.compare(password, user.password);
+      if (parsedCredentials.success) {
+        const { email, password } = parsedCredentials.data;
+        const user = await getUser(email);
+        if (!user) return null;
+        const passwordsMatch = await bcrypt.compare(password, user.password);
 
-          if (passwordsMatch) return user;
-
-          console.log('Invalid credentails');
-          return null;
-        }
+        if (passwordsMatch) return user;
+      }
+      return null;
     }
   })],
 });
